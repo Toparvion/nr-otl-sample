39dc38d900eb81876fb6908f7622838cf1d8b204
Toparvion <toparvion@gmx.com>
2023-11-13 07:50:48 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	
+	implementation(platform("io.opentelemetry:opentelemetry-bom-alpha:1.31.0-alpha"))
+	implementation(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:1.31.0-alpha"))
+	implementation("io.opentelemetry:opentelemetry-api")
+	implementation("io.opentelemetry:opentelemetry-sdk")
+	implementation("io.opentelemetry.instrumentation:opentelemetry-micrometer-1.5")
+	implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,68 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter
+import io.opentelemetry.instrumentation.micrometer.v1_5.OpenTelemetryMeterRegistry
+import io.opentelemetry.sdk.OpenTelemetrySdk
+import io.opentelemetry.sdk.metrics.Aggregation
+import io.opentelemetry.sdk.metrics.InstrumentType
+import io.opentelemetry.sdk.metrics.SdkMeterProvider
+import io.opentelemetry.sdk.metrics.export.AggregationTemporalitySelector
+import io.opentelemetry.sdk.metrics.export.DefaultAggregationSelector
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader
+import io.opentelemetry.sdk.resources.Resource
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Duration
+
+@SpringBootApplication
+class DemoApplication {
+  @Bean
+  fun openTelemetry(): OpenTelemetry {
+    return OpenTelemetrySdk.builder()
+      .setMeterProvider(
+        SdkMeterProvider.builder()
+          .setResource(
+            Resource.getDefault().toBuilder()
+              .put("service.name", "micrometer-shim")
+              // Include instrumentation.provider=micrometer to enable micrometer metrics experience in New Relic
+              .put("instrumentation.provider", "micrometer")
+              .build()
+          )
+          .registerMetricReader(
+            PeriodicMetricReader.builder(
+              OtlpGrpcMetricExporter.builder()
+                .setEndpoint("https://otlp.nr-data.net:4317")
+                .addHeader("api-key", "eu01xx6d8abea3e51651406bfbec8bd8FFFFNRAL") 
+                // IMPORTANT: New Relic requires metrics to be delta temporality
+                .setAggregationTemporalitySelector(
+                  AggregationTemporalitySelector.deltaPreferred()
+                ) // Use exponential histogram aggregation for histogram instruments
+                // to produce better data and compression
+                .setDefaultAggregationSelector(
+                  DefaultAggregationSelector.getDefault()
+                    .with(
+                      InstrumentType.HISTOGRAM,
+                      Aggregation.base2ExponentialBucketHistogram()
+                    )
+                )
+                .build()) // Match default micrometer collection interval of 60 seconds
+              .setInterval(Duration.ofSeconds(60))
+              .build())
+          .build())
+      .build()
+  }
+
+  @Bean
+  fun meterRegistry(openTelemetry: OpenTelemetry?): MeterRegistry {
+    return OpenTelemetryMeterRegistry.builder(openTelemetry).build()
+  }
+}
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
